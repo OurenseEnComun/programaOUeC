ab3d6d71acbbac8581b44fabdefb7c2be9214707
dani <monfero@gmail.com>
2015-03-22 22:40:33 +0100

    novos textos 4de5 a programa.js

--- programa-colaborativo_0.2/static/js/programa.js
+++ programa-colaborativo_0.2/static/js/programa.js
@@ -1,42 +1,50 @@
 var listobjetivos={
 "objetivos":[
 //modelo economico e enerxético
-    {"id":"vivenda digna",   
-     "titulo":"Garantir o acceso a unha vivenda digna ",
+    {"id":"modelo-economico",   
+     "titulo":"Modelo económico ",
      "apuesta":"modelo-economico-enerxetico",
-     "descripcion":"<p>Xente sen casas e casas sen xente?. Non pode ser. </p>"},
+     "descripcion":"<p>Propostas relacionadas co modelo económico. </p>"},
+    {"id":"modelo-enerxetico",   
+     "titulo":"Modelo enerxético ",
+     "apuesta":"modelo-economico-enerxetico",
+     "descripcion":"<p>Propostas relacionadas co modelo enerxético. </p>"},
+    {"id":"modelo-economico-enerxetico-xeral",   
+     "titulo":"Outras propostas relacionadas coa área ",
+     "apuesta":"modelo-economico-enerxetico",
+     "descripcion":"<p>Outras propostas relacionadas coa área. </p>"},
 
 //radicalización democrática
-    {"id":"radicalizacion-democratica-1",   
-     "titulo":"obxectivo 1 radicalizacion democrática ",
+    {"id":"radicalizacion-democratica-xeral",   
+     "titulo":"Propostas relacionadas coa radicalización democrática ",
      "apuesta":"radicalizacion-democratica",
-     "descripcion":"<p>Un dos obxectivos dentro da radicalización. </p>"},
+     "descripcion":"<p>Propostas relacionadas coa radicalización democrática. </p>"},
 
 
 //xustiza e cohesión social
-    {"id":"xustiza-cohesion-social-1", 
-    	"titulo":"obxectivo de xustiza e cohesión 1",
-    	"descripcion":"<p>Un dos obxectivos na xustiza e cohesión</p>.",
+    {"id":"xustiza-cohesion-social-xeral", 
+    	"titulo":"Propostas relacionadas coa xustiza e cohesión social",
+    	"descripcion":"<p>Propostas relacionadas coa xustiza e cohesión social</p>.",
         "apuesta":"xustiza-cohesion-social"}, 
 
 //modelo territorial e urbano
-    {"id":"modelo-territorial-urbano-1",	
-     "titulo":"Un obxectivo territorial",
+    {"id":"modelo-territorial-urbano-xeral",	
+     "titulo":"Propostas relacionadas co modelo territorial e urbano",
      "apuesta":"modelo-territorial-urbano",
-     "descripcion":"<p> Un dos obxectivos de territorio.</p>"},
+     "descripcion":"<p> Propostas relacionadas co modelo territorial e urbano.</p>"},
 
 
 // cultura-memoria-identidades
 
-    {"id":"cultura-memoria-identidades-1",   
-     "titulo":"Un obxectivo cultural",
+    {"id":"cultura-memoria-identidades-xeral",   
+     "titulo":"Propostas relacionadas con cultura, memoria e identidades",
      "apuesta":"cultura-memoria-identidades",
-     "descripcion":"<p> Un dos obxectivos de cultura.</p>"},
+     "descripcion":"<p> Propostas relacionadas con cultura, memoria e identidades.</p>"},
 
     {"id":"sen-categoria",   
-     "titulo":"Propuestas sin clasificar",
+     "titulo":"Propostas sen clasificar",
      "apuesta":"",
-     "descripcion":"En esta categoría agrupamos las medidas que los usuarios han enviado pero que no han sabido donde clasificar"}
+     "descripcion":"Nesta categorías agrupamos as medidas que usuarios e usuarias non souberon onde clasificar."}
 //
 
 ]}
@@ -45,11 +53,11 @@ var listobjetivos={
 var apuestas={
     'modelo-economico-enerxetico':{
         "titulo": "Modelo económico e enerxético",
-        "descripcion": "<p>A MAREA ATLÁNTICA aposta por un modelo económico sustentábel e enerxético renovábel :P. </p>",
+        "descripcion": "<p>Nun contexto de crise económica mundial, a Marea Atlántica quere comezar a construir outro paradigma dende a realidade local. Se o aumento do paro, a pobreza e a desigualdade non foron ata o de agora considerados motivos suficientes para o cambio, a perspectiva do esgotamento do petróleo e as enerxías fósiles debería ser o argumento definitivo para poñernos en marcha e iniciar unha transición cara unha economía sostible e xusta.</p><p>A Marea Atlántica defende medidas de aforro e a eficiencia nos máis diversos eidos da vida na cidade e da xestión municipal, e aposta decididamente polas enerxías renovables, limpas e descentralizadas. Ás necesarias auditorías de consumo de electricidade, coa conseguinte implantación de medidas técnicas de aforro, cómpre sumarlle novos conceptos de mobilidade de ordeación do territorio que favorezan a redución dos desprazamentos en coche.</p><p>Unha economía ao servizo das persoas require tamén reorientar as accións de fomento do emprego cara as necesidades urxentes da cidade: os servizos comunitarios, a rehabilitación, a prevención e xestión de residuos, o apoio ao pequeno comercio e á economía cooperativa e social.</p><p>A administración municipal debe ser exemplo de coherencia e apoio ás iniciativas locais e sostibles, de aí que deba aplicar criterios éticos nas súas compras e contratacións. As relacións do Concello coa banca e os poderes financieros tamén deben ser revisadas, priorizando as relacións coa banca ética e penalizando aqueles bancos e caixas que realicen execucións hipotecarias.</p>",
     },
     'radicalizacion-democratica':{
         "titulo": "Radicalización democrática",
-        "descripcion": "<p>A MAREA ATLÁNTICA aposta pola radicalización democrática. </p>",
+        "descripcion": "<p>A participación cidadá é un dos catro principios básicos da Marea Atlántica e como tal adquire un papel vertebrador de tódalas pospostas.</p><p>As mareantas sabemos que a democracia participativa é a condición necesaria para producir cambios sociais na nosa cidade nun sentido público e solidario. As transformacións, para producir xustiza e igualdade, requiren da meirande participación da cidadanía: a maior implicación do tecido social crítico e da veciñanza na xestión da cousa pública, maiores posibilidades de consolidar un cambio na correlación de forzas sociais que se traduza en propostas favorables á maioría social.</p><p>No bloque de participación cidadá e democracia queremos que se reflicta o desexo común de converter a democracia nun exercicio cotián, constante.</p><p>A Marea afirma a súa vontade de pular polos procesos de construción de democracia participativa dende abaixo, partindo do municipalismo. Participación cidadá que na medida en que creza vai producir a creación de contrapoderes sociais, quen de artellar un bloque social sólido para superar as eivas e insuficiencias da democracia representativa.</p><p>O goberno municipal incumpriu os mínimos de participación cidadá esixidos na Lei de grandes cidades, actúa contra a lei cando non da os pasos necesarios para poñer en marcha o Concello social, o Defensor do veciño nin desenvolve os Distritos urbanos como é a súa obriga. Ao incumprir esta lei o goberno está a impedir o control da súa acción política e privando á cidadanía da pluralidade dos mecanismos para reclamar os seus dereitos e o cumprimento do programa.</p><p>A Marea Atlántica persegue outra maneira de facer política, da xente e para a xente; de maneira limpa e transparente. O actual sistema de representación democrática semella deseñado para afastar a cidadanía da participación política e facilitar, deste xeito, a corrupción. </p><p>De acordo cos nosos Principios Básicos, o futuro goberno municipal da Marea Atlántica deberá favorecer que a toma de decisións no eido municipal se realice da forma máis participativa posíbel, axeitando os diversos mecanismos de participación ao alcance concreto de cada decisión ou decisións. En todo caso, as decisións máis relevantes, en termos de gasto público ou de creación de modelo de cidade, ou con significativo impacto urbanístico, social ou ambiental deben ser sometidas a cauces amplos de participación cidadá.</p><p>A Marea Atlántica adquiriu ademais o compromiso de promover e someter a aprobación do Pleno do Concello da Coruña, nos primeiros 100 días de mandato, unha reforma do Regulamento Orgánico e do Regulamento de Participación Cidadá, para deseñar as institucións e procedementos que permitan un verdadeiro goberno democrático, participativo e descentralizado da cidade. Neste futuro Regulamento Orgánico de Participación Cidadá activaranse formas de xestión e decisión descentralizada.</p>",
     },
     'xustiza-cohesion-social':{
         "titulo": "Xustiza e cohesión social",
@@ -57,10 +65,10 @@ var apuestas={
     },
     'modelo-territorial-urbano':{
         "titulo": "Modelo territorial e urbano",
-        "descripcion": "<p>A MAREA ATLÁNTICA aposta por un modelo territorial e urbano estupendérrimo.</p>",
+        "descripcion": "<p>Para a Marea a cidade é un ben común, un lugar onde relacionarnos libremente, onde medrar, aprender, namorar e disfrutar da vida de maneira colectiva. Por iso, a cidade debe deixar de ser o negocio duns poucos para convertirse nun espazo de dereitos para todas as persoas: dereito a un hábitat, á calidade de vida, á vivenda, á mobilidade... en definitiva, o dereito a unha vida digna.</p><p>A Cidade é, afortunadamente, unha realidade diversa onde todas as sensibilidades e modos de vida deben ter cabida, un espazo de respeto á diferencia: de xénero, funcional, xeracional, étnica... Debido á importancia e fraxilidade desta diversidade, as respostas aos problemas da Cidade non poden ser uniformes, senón adaptadas a cada realidade, e partindo sempre do reforzo das distintas formas de comunidade que constituen a riqueza da vida dos Barrios. Queremos unha cidade culta e popular, unha cidade saudable e sobretodo unha cidade xusta. Sen rendirnos aos dogmas da “humanización urbana” o as “smart cities”, dende a Marea traballaremos por unha cidade cohesionada e solidaria, cun asociacionismo forte, autónomo e independente das institucións, que poda traballar dende a vivencia e coñecemento de cada barrio por una cidade máis humana e intelixente.</p><p>Sabemos que unha cidade que respeta o medio ambiente é unha cidade con boa calidade de vida para a xente. Por iso queremos máis espazos verdes reais (non céspedes decorativos para encher estadísticas) e máis biodiversidade, unha auga máis limpia, un aire máis puro. Queremos unha cidade con menos ruido, menos coches, menos lixo, menos malos fumes. Queremos ser exemplo de aforro e de eficiencia, xestionando dende a economía de recursos e evitando o despilfarro con accións efectivas e oportunas, participativas e transparentes. Queremos protexer o noso patrimonio natural e arquitectónico herdado, conscientes de que é só un préstamo que nos fan as xeracións que están en camiño.</p><p>Chegou o momento de poñer fin a décadas de excesos urbanísticos, de especulación , perda de dereitos e crecemento da desigualdade. O modelo urbano ten que cambiar: unha cidade con 19.000 vivendas baleiras non pode seguir dilapidando o seu escaso territorio municipal con novo solo edificado. Temos que deixar atrás o desarrollismo edificatorio, interesado só no enriquecemento duns pocos, e camiñar cara a rexeneración e revitalización urbana. Traballar na pequena escala, na cicatrización das desfeitas consolidadas, na integración de usos e funcións, no enriquecemento e reforzo do existente.</p><p>Ainda temos unha Cidade con alma, a Marea chegou para coidala.</p>",
     },
     'cultura-memoria-identidades':{
         "titulo": "Cultura, memoria e identidades",
-        "descripcion": "<p>A MAREA ATLÁNTICA aposta por andar mais ao tanto e non tanto ao tonto.</p>",
+        "descripcion": "<p>Para a Marea Atlántica a cultura é un eixo vertebrador plenamente integrado na súa idea de construcción de A Coruña como cidade inclusiva, participada e crítica. A cultura entendida como un dereito e como unha práctica transformadora.</p><p>É por iso que a Marea ten o obxectivo de deseñar unha política cultural de cidade cunha praxe enfatizada tamén a medio e longo prazo, que faga innecesaria a dependencia ou particularidades dos axentes políticos. Unha política cultural baseada no diálogo, a interacción, a colaboración, a participación e a implicación cidadá.</p><p>Levamos tempo realizando unha análise que consinta e facilite unha definición de política cultural de cidade, unha diágnose que nos axude a activar un plan cultural amplo, aberto, concienzudo, artellado dende o coñecemento tanto do contexto como da pluralidade e os propios rasgos culturais existentes. Con este fin resulta necesario recuperar toda unha dimensión histórica, o legado da memoria, as pisadas de identidades e coñecementos acumulados presentes en tantos momentos na historia da nosa cidade.</p><p>Entendemos a cultura como un eixo transversal, á súa vez atravesado pola ética e o sentir da marea, nunca por intereses de elites nin como un ente pechado. Xamáis como acontece actualmente, conformada a modo de híbrido adscrito a unha costosa comisión de Festas carente de identidade. Fronte á política do evento executada sen máis horizontes que o interese económico e a discriminación da pluralidade, a cultura proposta pola Marea Atlántica é un espazo de acción e de transversalidades, é o espazo tamén da integración, da política de xénero e por suposto, da participación cidadá.</p><p>Nos ámbitos de actuación vinculados á cultura aparecen campos e conxuntos de saberes como a educación, a ciencia, as artes, o patrimonio, os deportes, a memoria, a mocidade, a lingua ou as festas. A clave pasa por xerar vínculos e desfacer os compartimentos estancos. Practicar unha economía dos recursos comúns e da interrelación de ámbitos e coñecementos.</p><p>O aproveitamento das infraestructuras e dos equipamentos municipais existentes así como o fomento da participación, a autoorganización e a vinculación coas propostas das creadoras e xestoras locais son accións que semellan razoables, axeitadas e asumibles para calquera acción de goberno neste eido.</p><p>A Marea levará a cabo un modelo de xestión marcado pola absoluta transparencia, o impulso da multiplicidade creativa, a ruptura coa política de privatización de recursos, o desenvolvemento dunha pedagoxía crítica e o apoio decidido á creación, innovación e investigación.</p><p>A activación de equipos de mediación cultural resulta clave neste contexto, ademáis de reflectir o noso ADN, un dos nosos factores diferenciais: e é que os modos e as formas son tamén a esencia dos contidos para a Marea Atlántica. Son indisociables.</p><p>En definitiva e en relación cos principios xerais que nos rixen: cómpre dotar de sentido, coherencia e ética todo plan cultural, así como fomentar a participación a todas as escalas, incluidas as relacións con outras institucións e axentes culturais. O apoio aos profesionais locais, raquítico hoxe, prefigura outra das necesidades. Todo este corpus de principios e medidas sinalan os fundamentos da nosa acción de goberno.</p><p>Unha revolución democrática móvese baixo o Parrote, a cidadanía está disposta a facer política cultural para a cidadanía.</p><p>O modelo faraónico treme.<br>A cultura quere ser practicada e disfrutada,<br>a memoria alimentada<br>e as identidades transversalizadas.</p>",
     }
 }
